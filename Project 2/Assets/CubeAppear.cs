843c072d23d5131770f0e449fe22f6b7325bfb64
JGoldsmith6321 <JGoldsmith6321@gmail.com>
2015-10-18 21:44:49 -0400

    part 1

---
+++ Project 2/Assets/CubeAppear.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeAppear : MonoBehaviour {
+	public GameObject cube;
+	public float x = -10.0f;
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < 16; i++){
+			Instantiate(cube,new Vector3(x,0,0),Quaternion.identity);
+			x = x + 1.1f;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Project 2/Assets/turnColor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnColor : MonoBehaviour {
+	public GameObject[] cubes;
+
+	// Use this for initialization
+	void Start () {
+		cubes = GameObject.FindGameObjectsWithTag ("cube");
+	}
+	void  OnMouseDown(){
+		foreach (GameObject aCube in cubes) {
+			aCube.GetComponent<Renderer>().material.color = Color.white;
+		}
+		GetComponent<Renderer>().material.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
