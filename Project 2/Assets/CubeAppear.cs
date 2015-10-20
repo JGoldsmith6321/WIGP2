8d2a84b5f96634a1f9c4b9f9015244c7298845c8
JGoldsmith6321 <JGoldsmith6321@gmail.com>
2015-10-19 20:16:45 -0400

    part 2

--- Project 2/Assets/CubeAppear.cs
+++ Project 2/Assets/CubeAppear.cs
@@ -3,16 +3,58 @@ using System.Collections;
 
 public class CubeAppear : MonoBehaviour {
 	public GameObject cube;
-	public float x = -10.0f;
+	public float xSpotStart = -8.25f;
+	public float xSpot;
+	public float ySpot = -4.4f;
+	public static int xGrid = 16;
+	public static int yGrid = 9;
+	public float moveBy = 1.1f;
+	public turnColor[,] allSpaces = new turnColor[xGrid,yGrid];
+	public int[] lastPlaneClicked;
+	public int[] lastActiveClicked = new int[]{0,8};
+	public bool readyMovePlane = false;
 
 	// Use this for initialization
 	void Start () {
-		for(int i = 0; i < 16; i++){
-			Instantiate(cube,new Vector3(x,0,0),Quaternion.identity);
-			x = x + 1.1f;
+		for (int y = 0; y < yGrid; y++) {
+			xSpot = xSpotStart;
+			for(int x = 0; x < xGrid; x++){// the x side
+				Vector3 pos = new Vector3(xSpot,ySpot,0);
+				GameObject thisCube = Instantiate(cube,pos,Quaternion.identity) as GameObject;
+				allSpaces[x,y] = thisCube.GetComponent<turnColor>();
+				int[] cords = new int[]{x,y};
+				allSpaces[x,y].SetCords(cords);
+				xSpot = xSpot + moveBy;
+			}
+			ySpot = ySpot + moveBy;
 		}
+		lastPlaneClicked = new int[]{0,8};
+		allSpaces[0,8].IsPlane(true);
 	}
+
+	public void ThingClicked (int[] cordsClicked){
+		bool thisSpacePlane = allSpaces [cordsClicked [0], cordsClicked [1]].isPlane;
+		bool thisSpaceActive = allSpaces [cordsClicked [0], cordsClicked [1]].isActive;
+
+		if (thisSpaceActive == false && thisSpacePlane == false && readyMovePlane == true) {//totally empty, and a plane is active
+			allSpaces [lastPlaneClicked [0], lastPlaneClicked [1]].IsPlane(false);//last plane space is empty
+			allSpaces [cordsClicked [0], cordsClicked [1]].IsPlane(true);//new plane space set
+			allSpaces [lastActiveClicked [0], lastActiveClicked [1]].IsActive(false);//last active spot off
+			lastPlaneClicked = cordsClicked;
+			readyMovePlane = false;
+		}
+		else if (thisSpacePlane == true && thisSpaceActive == true) {
+			allSpaces [cordsClicked [0], cordsClicked [1]].IsActive(false);
+			readyMovePlane = false;
+		}
+		else if (thisSpacePlane == true) {
+			allSpaces [cordsClicked [0], cordsClicked [1]].IsActive(true);
+			lastActiveClicked = cordsClicked;
+			readyMovePlane = true;
+		}
 	
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
--- Project 2/Assets/turnColor.cs
+++ Project 2/Assets/turnColor.cs
@@ -2,19 +2,48 @@
 using System.Collections;
 
 public class turnColor : MonoBehaviour {
-	public GameObject[] cubes;
+	public CubeAppear core;
+	public int[] cords;
+	public bool isPlane = false;
+	public bool isActive = false;
 
 	// Use this for initialization
 	void Start () {
-		cubes = GameObject.FindGameObjectsWithTag ("cube");
-	}
-	void  OnMouseDown(){
-		foreach (GameObject aCube in cubes) {
-			aCube.GetComponent<Renderer>().material.color = Color.white;
-		}
-		GetComponent<Renderer>().material.color = Color.red;
+		core = GameObject.FindGameObjectWithTag ("core").GetComponent<CubeAppear>();
 	}
 
+	void  OnMouseDown(){
+		core.ThingClicked(cords);
+	}
+
+	public void SetCords (int[] cordsGet){
+		cords = cordsGet;
+	}
+
+	public void IsActive (bool setActive){
+		isActive = setActive;
+		SetColor ();
+	}
+
+	public void IsPlane (bool plane){
+		isPlane = plane;
+		SetColor ();
+	}
+	void SetColor(){
+		if (isActive == true && isPlane == true) {
+			GetComponent<Renderer>().material.color = Color.magenta;
+		}
+		else if (isActive == true) {
+			GetComponent<Renderer>().material.color = Color.blue;
+		}
+		else if (isPlane == true) {
+			GetComponent<Renderer>().material.color = Color.red;
+		}
+		else{
+			GetComponent<Renderer>().material.color = Color.white;
+		}
+	}
+	
 	// Update is called once per frame
 	void Update () {
 	
